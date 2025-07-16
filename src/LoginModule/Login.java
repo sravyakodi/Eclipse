720bf0d5583cc42ff1f4e1afc944386522a29bc4
sravyakodi <sravyakodi2256@gmail.com>
2025-07-16 12:15:49 +0530

    updated

--- src/LoginModule/Login.java
+++ src/LoginModule/Login.java
@@ -3,6 +3,8 @@ package LoginModule;
 public class Login {
 
 	public static void main(String[] args) {
+		System.out.println("Username");
+		System.out.println("password");
 		System.out.println("login successful");
 
 	}
