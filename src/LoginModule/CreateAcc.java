3cf228576be7326bd969ecb0d314d660cd76f184
sravyakodi <sravyakodi2256@gmail.com>
2025-07-16 12:12:21 +0530

    Edit

--- src/LoginModule/CreateAcc.java
+++
@@ -1,9 +0,0 @@
-package LoginModule;
-
-public class CreateAcc {
-
-	public static void main(String[] args) {
-		System.out.println("Account is created successfully");
-	}
-
-}
---
+++ src/LoginModule/Login.java
@@ -0,0 +1,10 @@
+package LoginModule;
+
+public class Login {
+
+	public static void main(String[] args) {
+		System.out.println("login successful");
+
+	}
+
+}
